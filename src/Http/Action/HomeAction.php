8a427f2cc2acb6e2503208924a66a35974ce2093
Vadim Korostelev <vdmkbu@ya.ru>
2020-06-26 22:44:38 +0500

    контроллеры реализуют интерфейс RequestHandler

--- src/Http/Action/HomeAction.php
+++ src/Http/Action/HomeAction.php
@@ -5,14 +5,12 @@ namespace App\Http\Action;
 use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
-    public function __invoke(
-        ServerRequestInterface $request,
-        ResponseInterface $response
-    ): ResponseInterface {
-
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
         return new JsonResponse([
             'name' => 'API',
             'version' => '1.0'
--- src/Http/Action/UploadAction.php
+++ src/Http/Action/UploadAction.php
@@ -5,14 +5,12 @@ namespace App\Http\Action;
 use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class UploadAction
+class UploadAction implements RequestHandlerInterface
 {
-    public function __invoke(
-        ServerRequestInterface $request,
-        ResponseInterface $response
-    ): ResponseInterface {
-
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
         $data = $request->getParsedBody();
 
         return new JsonResponse($data);

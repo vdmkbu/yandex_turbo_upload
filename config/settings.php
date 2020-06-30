df69e7d3f45b92611e15bb3af0a2f7fb457e29e0
Vadim Korostelev <vdmkbu@ya.ru>
2020-06-30 22:02:26 +0500

    настройки для PDO

--- config/settings.php
+++ config/settings.php
@@ -21,4 +21,26 @@ $settings['error'] = [
     'log_error_details' => true,
 ];
 
+$settings['db'] = [
+    'driver' => 'mysql',
+    'host' => getenv('DB_HOST'),
+    'username' => getenv('DB_USER'),
+    'database' => getenv('DB_NAME'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ],
+];
+
 return $settings;

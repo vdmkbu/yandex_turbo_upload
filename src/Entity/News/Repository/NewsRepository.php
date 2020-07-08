f5ab99cc15ace91661f31c8cba2712df572855cf
Vadim Korostelev <vdmkbu@ya.ru>
2020-07-08 20:02:09 +0500

    установка twig

---
+++ src/Entity/News/Repository/NewsRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vdmkbu
+ * Date: 01.07.20
+ * Time: 20:57
+ */
+
+namespace App\Entity\News\Repository;
+
+
+class NewsRepository
+{
+
+}
---
+++ src/Entity/News/Repository/NewsRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vdmkbu
+ * Date: 01.07.20
+ * Time: 20:47
+ */
+
+namespace App\Entity\News\Repository;
+
+
+interface NewsRepositoryInterface
+{
+
+}

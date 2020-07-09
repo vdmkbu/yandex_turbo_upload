64abcaf6be8b6c1403a5088e13cfb1c50a5f3e5c
Vadim Korostelev <vdmkbu@ya.ru>
2020-07-09 21:32:04 +0500

    контроллер для upload

--- src/Http/Action/UploadAction.php
+++ src/Http/Action/UploadAction.php
@@ -3,15 +3,29 @@
 namespace App\Http\Action;
 
 use App\Http\JsonResponse;
+use App\Service\UploadService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+
 class UploadAction implements RequestHandlerInterface
 {
+
+    private $service;
+
+    public function __construct(UploadService $service)
+    {
+        $this->service = $service;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $data = $request->getParsedBody();
+
+        $feed = $this->service->upload($data);
+
+        echo $feed;
 
         return new JsonResponse($data);
     }

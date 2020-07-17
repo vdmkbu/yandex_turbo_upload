d207c54b96806374461a8b8481a8f2dcfdd71728
Vadim Korostelev <vdmkbu@ya.ru>
2020-07-17 15:31:25 +0500

    тесты запросов к /upload, /delete

--- tests/Http/RequestTest.php
+++ tests/Http/RequestTest.php
@@ -35,4 +35,101 @@ class RequestTest extends WebTestCase
         self::assertEquals('API', $response['name']);
         self::assertEquals('1.0', $response['version']);
     }
+
+    /** @test */
+    public function test_empty_messages()
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'X-API-TOKEN' => getenv('AUTH_TOKEN')
+        ];
+
+        $this->expectException(ClientException::class);
+
+        $response = $this->client($headers)->request('POST', '/upload', []);
+        self::assertEquals(422, $response->getStatusCode());
+
+        $response = $this->client($headers)->request('POST', '/delete', []);
+        self::assertEquals(422, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function test_success_upload_not_prod()
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'X-API-TOKEN' => getenv('AUTH_TOKEN')
+        ];
+
+        $response = $this->client($headers)->request('POST', '/upload', [
+            'form_params' => [
+                'messages' => [108351],
+                'prod' => 0
+            ]
+        ]);
+
+        $response = json_decode($response->getBody()->getContents(), true);
+
+        self::assertNotEmpty($response['feed']);
+    }
+
+    /** @test */
+    public function test_success_upload_prod()
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'X-API-TOKEN' => getenv('AUTH_TOKEN')
+        ];
+
+        $response = $this->client($headers)->request('POST', '/upload', [
+            'form_params' => [
+                'messages' => [108351],
+                'prod' => 1
+            ]
+        ]);
+
+        $response = json_decode($response->getBody()->getContents(), true);
+
+        self::assertNotEmpty($response['task_id']);
+    }
+
+    /** @test */
+    public function test_success_delete_not_prod()
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'X-API-TOKEN' => getenv('AUTH_TOKEN')
+        ];
+
+        $response = $this->client($headers)->request('POST', '/delete', [
+            'form_params' => [
+                'messages' => [108351],
+                'prod' => 0
+            ]
+        ]);
+
+        $response = json_decode($response->getBody()->getContents(), true);
+
+        self::assertNotEmpty($response['feed']);
+    }
+
+    /** @test */
+    public function test_success_delete_prod()
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'X-API-TOKEN' => getenv('AUTH_TOKEN')
+        ];
+
+        $response = $this->client($headers)->request('POST', '/delete', [
+            'form_params' => [
+                'messages' => [108351],
+                'prod' => 1
+            ]
+        ]);
+
+        $response = json_decode($response->getBody()->getContents(), true);
+
+        self::assertNotEmpty($response['task_id']);
+    }
 }
